137d8b9cfd6697113be77dedc5b6dec9a88892af
Raveesh Kumar <92170670+Raveesh1007@users.noreply.github.com>
2024-11-04 20:53:59 +0530

    logos and votingbutton

---
+++ src/app/Components/ui/votingbutton.tsx
@@ -0,0 +1,35 @@
+"use client"
+import React, {useState} from "react";
+import axios from "axios";
+import Chart from "./chart"
+
+export function VotingButton({
+    crypto,
+    votes,
+}:{
+    crypto: string,
+    votes:{bullish: number, bearish: number},
+}){
+    const [disabled, setDisabled] = useState(false);
+    const [error, seterror] = useState(false);
+    const [voted, setvoted] = useState(false);
+
+    async function handleClick(direction: string){
+        const date = new Date().getDate();
+        if(date === parseInt(localStorage.getItem(crypto)!)){
+            setvoted(true);
+            setDisabled(true);
+            return;
+        }
+        localStorage.setItem(crypto, `${date}`)
+
+        let bullishValue = 0;
+        let bearishValue = 0;
+
+        if(direction === "bullish"){
+            bullishValue = 1;
+        }else(direction === "bearish"){
+            bearishValue = 1;
+        }
+    }
+}
