8d6ee9e30f4a04851518ee05396578820df68101
Raveesh Kumar <92170670+Raveesh1007@users.noreply.github.com>
2024-11-06 17:09:01 +0530

    Navbar

--- src/app/Components/ui/Navbar.tsx
+++ src/app/Components/ui/Navbar.tsx
@@ -1,1 +1,13 @@
 import { FloatingNav } from "./NavBar-menu";
+export function Navbar(){
+    const NavItems = [
+        {
+            title: "Home",
+            link: "/",
+            icon: (
+                <svg 
+                    xmlns = "http://www.w3.org/2000/svg"
+            )
+        }
+    ]
+}
