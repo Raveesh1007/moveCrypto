b794a973cbeb7be186cdee766ae9666b597c88ae
Raveesh Kumar <92170670+Raveesh1007@users.noreply.github.com>
2024-11-07 12:15:12 +0530

    BTCapi route created Successfuly

---
+++ src/app/api/btc/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import dbconnect from "../../../app/utils/dbconnect";
+import { BTCschema } from "@/app/models/schema";
+import voteAssign from "@/app/utils/assignvote";
+
+export async function POST(req: NextRequest){
+    const body = await req.json();
+
+    try{
+        await dbconnect();
+        const {bullish, bearish} = body;
+        const {bull, bear} = voteAssign(bullish, bearish);
+        await BTCschema.updateOne({
+            $inc:{
+                bullish: bear,
+                bearish: bear,
+            },
+        });
+        return NextResponse.json({meassage: "Created Successfully"});
+    } catch(e){
+        return NextResponse.json({Error: "Error Occured"});
+    }
+}
