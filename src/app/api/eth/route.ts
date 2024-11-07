7010112e9dd34ead0be6a6edb5d0f610627fd2ca
Raveesh Kumar <92170670+Raveesh1007@users.noreply.github.com>
2024-11-07 12:17:57 +0530

    ETHroute successfully created

---
+++ src/app/api/eth/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import dbconnect from "../../../app/utils/dbconnect";
+import { ETHschema } from "@/app/models/schema";
+import voteAssign from "@/app/utils/assignvote";
+
+export async function POST(req: NextRequest){
+    const body = await req.json();
+
+    try{
+        await dbconnect();
+        const {bullish, bearish} = body;
+        const {bull, bear} = voteAssign(bullish, bearish);
+        await ETHschema.updateOne({
+            $inc:{
+                bullish: bull,
+                bearish: bear,
+            },
+        });
+        return NextResponse.json({meassage: "Created Successfully"});
+    } catch(e){
+        return NextResponse.json({Error: "Error Occured"});
+    }
+}
