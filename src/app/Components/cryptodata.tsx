cf849a40e00579de4a0cecf63fe2742d179ec065
Raveesh Kumar <92170670+Raveesh1007@users.noreply.github.com>
2024-11-09 18:13:05 +0530

    cryptodata.tsx

---
+++ src/app/Components/cryptodata.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from "react"
+import { CardBody, CardContainer, CardItem } from "./ui/3dcard"
+import { BTCLogo,SOLlogo,ETHlogo } from "./ui/logos"
+
+interface Datatypes{
+    price: number,
+    crypto: string
+}
+
+export function CryptoData(){
+    
+}
