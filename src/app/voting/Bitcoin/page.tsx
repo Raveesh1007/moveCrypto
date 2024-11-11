e72a897d2635d5fc81ad68523fca88677121e897
Raveesh Kumar <92170670+Raveesh1007@users.noreply.github.com>
2024-11-11 19:44:37 +0530

    btc coin page

---
+++ src/app/voting/Bitcoin/page.tsx
@@ -0,0 +1,23 @@
+import { fetchCryptoData } from "@/app/utils/fetchcrypto";
+import CryptoPage from "../../Components/CryptoPage";
+import { BTCschema } from "@/app/models/schema";
+
+export default async function Bitcoin() {
+    try {
+      const { votes, price } = await fetchCryptoData{BTCschema, "bitcoin");
+      return (
+        <CryptoPage
+          crypto="Bitcoin"
+          price={price}
+          votes={votes}
+          cryptoKey="btc"
+        />
+      );
+    } catch (e: any) {
+      return (
+        <div className="absolute left-0 top-10">
+          Failed to fetch Bitcoin data. Please try again later. {e.message}
+        </div>
+      );
+    }
+  }
